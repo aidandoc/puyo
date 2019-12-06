c3291f7af2fb3d6fd1e0d562e11cbc5489a25de1
aidan <aidand@friends-select.org>
2019-12-06 10:59:02 -0500

    big brain

--- main.lua
+++ main.lua
@@ -1,113 +1,144 @@
-local module = require("module")
-local class  = require("class")
-local width,height = math.floor(love.graphics.getWidth()/5),math.floor(love.graphics.getHeight()*0.8)
-local grid = setmetatable({},module.meta)
-local bricks = setmetatable({},module.meta)
-local offset = 50
-local a,b = 20,20
-local bStart = {
+module = require("module")
+class  = require("class")
+
+grid = setmetatable({},module.meta)
+bricks = setmetatable({},module.meta)
+
+offset = 50
+sizeX,sizeY = 20,20
+width,height = math.floor(love.graphics.getWidth()/5),math.floor(love.graphics.getHeight()*0.8)
+
+down = false
+frameFall = 120
+position = {
     x = width,
     y = 10
 }
-local frameProp = {
-    fall = 120,
-    move = 20
-}
-local stuck = false -- testing purposes
-local down = false
 
-function generateGrid()
-    for f = 1, math.floor(height/b) do
-        for i = 0, math.floor(width/a) - 1 do
-            grid({(i * a) + width, (f * b) - (b - offset), false})
+function newPuyo()
+    rect = {}
+    rect.x = position.x
+    rect.y = position.y
+    rect.width = sizeX
+    rect.height = sizeY
+    rect.stuck = false
+
+    table.insert(puyos, rect)
+end
+
+
+function generateGrid() -- check if errors
+    for columns = 1, math.floor(height/sizeY) do
+        for rows = 0, math.floor(width/sizeX) - 1 do
+            props = {}
+            props.x = (columns * sizeX) + width
+            props.y = (rows * sizeY) - (sizeX - offset)
+            props.occ = false
+            grid(props)
         end
     end
 end
 
-function newBlock()
-    block = class.rect:new(bStart.x,bStart.y,a,b)
-    block:prop("stuck",0)
-    return block
-end
-
 function love.load()
-    frames = 0
 
     love.graphics.setBackgroundColor(255,128/255,128/255)
-    test_rect = class.rect:new(10,10,200,300)
-    test_rect:prop("occ",0)
-
     l_bound = class.rect:new(width - width/4, offset, width/4, height)
     r_bound = class.rect:new(width*2, offset, width/4, height)
     ground = class.rect:new(width - width/4,height + offset,width + width/2,offset)
 
-    generateGrid()
-end
-function love.update(t) 
-    frames = frames + 1 -- amount of frames
 
-    if frames % frameProp.fall == 0 then
-        if not down then
-            if not stuck then
-                bStart.y = bStart.y + b 
+    generateGrid()
+
+    puyos = {}
+    frames = 0
+
+end
+
+function love.update(dt)
+    frames = frames + 1
+
+    for i,v in ipairs(puyos) do
+        if not v.stuck then
+            if frames % frameFall == 0 then
+                if not down then
+                    v.y = v.y + sizeY/2
+                end
             end
         end
     end
 
-    
-    function love.keypressed(k)
-        if k == "escape" then
+
+    function love.keypressed(key)
+        if key == "escape" then
             love.event.quit()
         end
-        if k == "e" then
-            print(grid)
-        end
-        if k == "left" then
-            if not stuck then
-                bStart.x = bStart.x - a
+        if key == "left" then -- check for blocks
+            for i,v in ipairs(puyos) do
+                if not v.stuck then
+                    v.x = v.x - sizeX
+                end
             end
         end
-        if k == "right" then
-            if not stuck then
-                bStart.x = bStart.x + a
+        if key == "right" then
+            for i,v in ipairs(puyos) do
+                if not v.stuck then
+                    v.x = v.x + sizeX
+                end
             end
+        end
+        if key == "space" then
+            newPuyo()
         end
     end
 
     if love.keyboard.isDown("down") then
         down = true
-        bStart.y = bStart.y + 1
+        for i,v in ipairs(puyos) do
+            if not v.stuck then
+                v.y = v.y + 1
+            end
+        end
     end
 
-    function love.keyreleased(k)
-        if k == "down" then
+    function love.keyreleased(key)
+        if key == "down" then
             down = false
         end
     end
 
-    if bStart.y + b >= height+offset then -- ground
-        bStart.y = height+offset - b
-        stuck = true
+    -- borders
+    for i,v in ipairs(puyos) do
+        if v.y + sizeY >= height + offset then -- ground
+            v.y = height + offset - sizeY
+            v.stuck = true
+        end
     end
 
-    if bStart.x + b >= width*2 then -- left bound
-        bStart.x = width*2 - b
+    for i,v in ipairs(puyos) do
+        if v.x + sizeX >= width * 2 then -- left bound
+            v.x = width * 2 - sizeX
+        end
     end
 
-    if bStart.x - b <= width - width/4 then -- right bound
-        bStart.x = (width - width/4) + b*2
+    for i,v in ipairs(puyos) do
+        if v.x - sizeX <= width - width/4 then -- right bound
+            v.x = (width - width/4) + sizeX * 2
+        end
     end
 
 end
 
 function love.draw()
+
     love.graphics.setColor(1,1,1)
 
-    love.graphics.rectangle("line",width,50,width,math.floor(height))
-    r_bound:draw("fill")
-    l_bound:draw("fill")
-    ground:draw("fill")
+    r_bound:draw("line")
+    l_bound:draw("line")
+    ground:draw("line")
 
-    love.graphics.setColor(128/255,128/255,1)
-    newBlock(0,0):draw("fill")
+    love.graphics.setColor(128/255,128/255,255)
+
+    for i,v in ipairs(puyos) do
+        love.graphics.rectangle("fill",v.x,v.y,v.width,v.height)
+    end
 end
